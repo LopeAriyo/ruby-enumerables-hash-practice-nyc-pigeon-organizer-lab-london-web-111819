6b1ef9e1d92ac3f67fe5e1e92517b7f65ec47405
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-08 21:16:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,5 +9,27 @@ def nyc_pigeon_organizer(data)
             "Alex" => {}}
   # write your code here!
 
+  data.each do |key, value| #keys from data are colour, gender, lives
+    value.each do |describe, array|
+      array.each do |name| 
+
+        if pigeons.has_key?(name) # checks if the pigeons hash has all necessary names added
+          if pigeons[name].has_key?(key) # checks if each pigeons has key 
+          
+            pigeons[name][key] << describe.to_s
+            #if the name 
+          else
+            pigeons[name][key] = [describe.to_s]
+          end
+          
+        else
+          pigeons[name] = {key => describe.to_s} #creates an entry for the missing pigeon plus key value data
+        end
+
+      end
+    end
+  end
+  
   pigeons
+  
 end
