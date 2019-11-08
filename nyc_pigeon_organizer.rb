9daaf4975767a54bae98b702056432fddfe3f0cb
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-08 20:47:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,13 @@
 def nyc_pigeon_organizer(data)
 
-  result = {"Theo" => {}, "Peter Jr." => {}, "Lucky" => {}, "Ms. K" => {}, "Queenie" => {}, "Andrew" => {}, "Alex" => {}}
+  pigeons = {"Theo" => {}, 
+            "Peter Jr." => {}, 
+            "Lucky" => {}, 
+            "Ms. K" => {}, 
+            "Queenie" => {}, 
+            "Andrew" => {}, 
+            "Alex" => {}}
   # write your code here!
 
-  result
+  pigeons
 end
